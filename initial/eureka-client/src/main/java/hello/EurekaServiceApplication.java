715c19bead4d53c296d207a490c2a0ffa7f7b05e
dvelopp <dvelopp@gmail.com>
2018-11-23 22:47:11 +0200

    Initial commit

---
+++ initial/eureka-client/src/main/java/hello/EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
